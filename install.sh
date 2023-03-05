ffe9b7df35c8aa37e5f3876ed02b302293fd5308
Harsh Buddhadev <buddhadevharsh953@gmail.com>
2023-03-05 22:22:18 +0530

    Restructured Files

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+#	python 3
+sudo apt update && sudo apt upgrade -y || "Error Updating"
+sudo apt install python3 || "Error Installing Python3"
+#	pip
+sudo apt install python3-pip || "Error Installing Pip"
+#	Google API
+pip3 install google-auth-oauthlib || "Error installing google-auth-oauthlib"
+pip3 install google-api-python-client || "Error Installing google-api-python-client"
+pip3 install google-auth || "Error installing google-auth"
+#	LCD
+pip3 install adafruit-charlcd || "Error Installing adafruit-charlcd"
+#	Fingerprint
+pip3 install adafruit-circuitpython-fingerprint || "Error Installing adafruit-circuitpython-fingerprint"
+#	Keypad
+pip3 install adafruit-circuitpython-matrixkeypad || "Error Installing adafruit-circuitpython-matrixkeypad"

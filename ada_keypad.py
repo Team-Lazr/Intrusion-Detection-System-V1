d9ffb09d4ffb0d602f4f49610431ad6f0e4ebaa8
harsh.buddhadev@omen15 <buddhadevharsh953@gmail.com>
2023-06-24 19:04:12 +0530

    Added Comments and Updated Main Section

--- ada_keypad.py
+++ ada_keypad.py
@@ -3,17 +3,20 @@ import digitalio
 import board
 import adafruit_matrixkeypad
 
+#Init Keypad GPIO Pins
 rows = [digitalio.DigitalInOut(x) for x in (board.D25, board.D8, board.D7, board.D1)]
 cols = [digitalio.DigitalInOut(x) for x in (board.D12, board.D16, board.D20, board.D21)]
+
+#Declare Keypad Pattern Here
 keys = (("1", "2", "3", "A"),
-        ("4", "5", "6", "B"),
-        ("7", "8", "9", "C"),
-        ('*', "0", '#', "D"))
+		("4", "5", "6", "B"),
+		("7", "8", "9", "C"),
+		('*', "0", '#', "D"))
 
 keypad = adafruit_matrixkeypad.Matrix_Keypad(rows, cols, keys)
 
 def hearing():
-	keys = keypad.pressed_keys
+	keys = keypad.pressed_keys #Check what key was Pressed
 	# print("hearing in ada called")
 	if keys :
 		# print("entered if from keys")
@@ -24,23 +27,20 @@ def hearing():
 				time.sleep(0.15)
 				# print("return from ada =",char[0])
 				return char[0]
-				break
 	else:
 		time.sleep(0.1)
 		return None	 
-
-	
-
-
 		
-
-# while True:
-#     # print(hearing())
-# 	getpin()
-
-
-# while True:
-# 	b= hearing()
-# 	if (b!= None):
-# 		print (b)
-# 		# break
+if __name__ == "__main__":
+	while True:
+		try: 
+			b= hearing()
+			if (b!= None):
+				print (b)
+				# break
+		except KeyboardInterrupt:
+			print("Keyboard Interupt")
+			exit()
+		except Exception as e:
+			print(f"Exception Occured : {e}")
+			exit()

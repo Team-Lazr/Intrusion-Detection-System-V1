53f577a49496fdf91d9b6a8557895115ea73f475
harsh.buddhadev@omen15 <buddhadevharsh953@gmail.com>
2023-06-24 19:05:17 +0530

    Added Comments

--- upload.py
+++ upload.py
@@ -11,31 +11,27 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 # a = str(dt_object)
 dt_now = (datetime.now().strftime('%b %d %Y , %H:%M'))
 
-
-
-
-
 def sheetsupdate(task,method,remark):
 	try:
 		CLIENT_SECRET_FILE_data_log = 'creds.json'
 		service_data_log = Create_Service(CLIENT_SECRET_FILE_data_log, API_NAME, API_VERSION, SCOPES)
 
-		spreadsheet_id_data_log = '12iDVTvh36UQTMPm-Hz4_SaF4OG5Fqt2eTq_-RUIME2s'
+		spreadsheet_id_data_log = '12iDVTvh36UQTMPm-Hz4_SaF4OG5Fqt2eTq_-RUIME2s' #Enter Google Sheet ID Here
 		mySpreadsheets = service_data_log.spreadsheets().get(spreadsheetId=spreadsheet_id_data_log).execute()
 
-		worksheet_name = 'Sheet1!'
-		cell_range_insert = 'A1'
-		values = ((dt_now,task,method,remark),())
+		worksheet_name = 'Sheet1!' #Enter Worksheet Name
+		cell_range_insert = 'A1' #Enter Starting Rangee
+		values = ((dt_now,task,method,remark),()) #Pass Values here
 
 		value_range_body = {
 		'majorDimension': 'ROWS',
 		'values': values
 		}
 		service_data_log.spreadsheets().values().append(
-		spreadsheetId=spreadsheet_id_data_log,
-		valueInputOption='USER_ENTERED',
-		range=worksheet_name + cell_range_insert,
-		body=value_range_body
+			spreadsheetId=spreadsheet_id_data_log,
+			valueInputOption='USER_ENTERED',
+			range=worksheet_name + cell_range_insert,
+			body=value_range_body
 		).execute()
 
 		print("End of Updating Sheets")

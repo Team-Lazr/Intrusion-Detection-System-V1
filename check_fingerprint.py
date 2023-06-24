7fb6b55ad529b156234b175fc6217731e54d1899
harsh.buddhadev@omen15 <buddhadevharsh953@gmail.com>
2023-06-24 19:04:44 +0530

    Updated Serial Init,Tab Spacing and main Section

--- check_fingerprint.py
+++ check_fingerprint.py
@@ -3,22 +3,25 @@ import board
 from digitalio import DigitalInOut, Direction
 import adafruit_fingerprint
 import serial
-uart = serial.Serial("/dev/serial0", baudrate=57600, timeout=1)
+
+port="/dev/serial0"
+uart = serial.Serial(port, baudrate=57600, timeout=1) #Init Serial Port
 finger = adafruit_fingerprint.Adafruit_Fingerprint(uart)
 
 def get_fingerprint():
-    if finger.read_templates() != adafruit_fingerprint.OK:
-        raise RuntimeError("Failed to read templates")
-    while finger.get_image() != adafruit_fingerprint.OK:
-        pass
-    if finger.image_2_tz(1) != adafruit_fingerprint.OK:
-        return False
-    if finger.finger_search() != adafruit_fingerprint.OK:
-        return False
-    return True
+	if finger.read_templates() != adafruit_fingerprint.OK:
+		raise RuntimeError("Failed to read templates")
+	while finger.get_image() != adafruit_fingerprint.OK:
+		pass
+	if finger.image_2_tz(1) != adafruit_fingerprint.OK:
+		return False
+	if finger.finger_search() != adafruit_fingerprint.OK:
+		return False
+	return True
  
-# while True:
-# 	if get_fingerprint():
-# 		print("Detected fingerprint")
-# 	else:
-# 		print("Finger not found")
+if __name__ == "__main__":
+	while True:
+		if get_fingerprint():
+			print("Detected fingerprint")
+		else:
+			print("Finger not found")
